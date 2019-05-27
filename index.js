146a01e5ca825406db601a06ef08a03f1e5b614c
VAMSI <vamsikrishnareddybevara@gmail.com>
2019-05-27 16:47:52 +0530

    Number parser update 5

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const booleanParser = input => (!input.startsWith('true') && !input.startsWith('
 
 
 // Number Parser
-let numRegex = /^[-]?[0-9]\d*(\.\d+)?([eE]?[+-]\d+)?/;
+let numRegex = /^[-]?[0-9]\d*(\.\d+)?([eE]?[+-]?\d+)?/;
 const numberParser = input => {
-  if( input === '0') return [0,''];
-  if(input[0] === '0' && input.length !== 1) return null;
   let secondValue = input.slice(input.match(numRegex)[0].length);
+  if( input === '0') return [0,''];
+  if(input[0] === '0' && input.length !== 1 && ! /\./.test(input)) return null;
+  
   return !numRegex.test(input)? null: (secondValue[0] === 'e' || secondValue[0] === 'E' || secondValue[0] === '.')? null: [(parseFloat(input.match(numRegex)[0])), secondValue];
 }
+
+
 // String Parser
 const stringparser = input => 
