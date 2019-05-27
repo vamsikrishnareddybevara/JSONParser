f8e8ba9e9e216feb4c87f4071e48e6465dbeb765
VAMSI <vamsikrishnareddybevara@gmail.com>
2019-05-27 14:45:57 +0530

    Number Parser correct solution

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+// Null Parser
 const nullParser = input => (!input.startWith('null'))? null: [null, input.slice(4)];
-const booleanParser = input => (!input.startsWith('true') || !input.startsWith('false'))? null:0 (input.startsWith('true'))? [true, input.slice(4)]: [false, input.slice(5)];
-let numRegex = /^[+-]?[0-9]\d*(\.\d+)?/;
-const numberParser = input => !numRegex.test(input)? null: [(parseFloat(input.match(numRegex)[0])), input.slice(input.match(numRegex)[0].length)];
+
+// Boolean Parser
+const booleanParser = input => (!input.startsWith('true') && !input.startsWith('false'))? null: (input.startsWith('true'))? [true, input.slice(4)]: [false, input.slice(5)];
+let numRegex = /^[+-]?[1-9]\d*(\.\d+)?(([eE]?)([+-])?\d+)?/;
+
+// Number Parser
+const numberParser = input => {
+  if( input === '0') return [0,''];
+  return !numRegex.test(input) && input !== "0"? null: [(parseFloat(input.match(numRegex)[0])), input.slice(input.match(numRegex)[0].length)];
+}
+
+// String Parser
+const stringparser = input => 
