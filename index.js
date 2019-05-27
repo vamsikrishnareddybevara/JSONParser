dd293cebe05e90c68203361827b18c05cdcb0fdc
VAMSI <vamsikrishnareddybevara@gmail.com>
2019-05-27 12:17:12 +0530

    Boolean and Num Parser

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const nullParser = input => (!input.startWith('null'))? null: [null, input.slice(4)];
-const booleanParser = input => (!input.startsWith('true') || !input.startsWith('false'))? null: (input.startsWith('true'))? [true, input.slice(4)]: [false, input.slice(5)];
+const booleanParser = input => (!input.startsWith('true') || !input.startsWith('false'))? null:0 (input.startsWith('true'))? [true, input.slice(4)]: [false, input.slice(5)];
+let numRegex = /^[+-]?[0-9]\d*(\.\d+)?/;
+const numberParser = input => !numRegex.test(input)? null: [(parseFloat(input.match(numRegex)[0])), input.slice(input.match(numRegex)[0].length)];
