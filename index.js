da6837ff205e4e5cf165427209d247da9e10c8c3
VAMSI <vamsikrishnareddybevara@gmail.com>
2019-06-01 19:04:26 +0530

    JSON parser Working

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// Null Parser
 const fs = require('fs');
+// Null Parser
 const nullParser = input => (!input.startsWith("null"))? null: [null, input.slice(4)];
 
 // Boolean Parser
@@ -8,15 +8,12 @@ const booleanParser = input => (!input.startsWith('true') && !input.startsWith('
 // Number Parser
 let numRegex = /^[-]?[0-9]\d*(\.\d+)?([eE]?[+-]?\d+)?/;
 const numberParser = input => {
-  	if( input === '0') return [0,''];
-  	if(input[0] === '0' && input.length !== 1 && ! /\./.test(input)) return null;
-  	if(!numRegex.test(input)){
-  		return null;
-  	}else {
-  	let secondValue = input.slice(input.match(numRegex)[0].length);
-  	return (secondValue[0] === 'e' || secondValue[0] === 'E' || secondValue[0] === '.')? null: [(parseFloat(input.match(numRegex)[0])), secondValue];
-  }
-  return null;
+	  if(!numRegex.test(input)) return null;
+	  if(input[0] === '0' && input.match(numRegex)[0].split(".").length !== 1) return null;
+	  else {
+		  let secondValue = input.slice(input.match(numRegex)[0].length);
+	  	  return (secondValue[0] === 'e' || secondValue[0] === 'E' || secondValue[0] === '.')? null: [(parseFloat(input.match(numRegex)[0])), secondValue];
+	  }
 }
 
 // String Parser
@@ -40,67 +37,56 @@ let escapeCharacters = {
     "\t": true
 }
 
+
 const stringParser = str => {
-	let stringValue = "";		
+	let stringValue = "";
 	let quoteCount = 0;
 	for(let i = 0; i < str.length; i++) {
-		if(str[0] !== "\"") return null;
-		if(str[i] === "\"") {
-			quoteCount++;
-		}
-
+		if(str[i] === "\"") quoteCount++;
 		if(escapeCharacters[str[i]]) return null;
 	    if(str[i] === "\\") {
 	      if( str[i+1] === 'u') {
 	        if(!unicodeRegex.test(str.slice(i+2, i+6))) return null;
 	        else {
-	          stringValue = stringValue + "\\u" + parseInt(str.slice(i+2, i+6));
+	          stringValue = stringValue + String.fromCharCode(parseInt(str.slice(i+2, i+6),16));
 	          i = i+5;
 	        }
 	      } else {
 	      	      if(!specialCharacters[str[i+1]]) return null;
-			      else {
-			      stringValue = stringValue + specialCharacters[str[i+1]];
-			      i = i + 1;
-			      }
+			      else stringValue = stringValue + specialCharacters[str[i+1]];
+			      i++;
 	    	}
 	    }
 	    else {
 	      stringValue += str[i];
 	    }
-	    if (quoteCount === 2) return [stringValue.slice(1,-1), str.slice(stringValue.length)] ;
-	  }
-}
+	    if(quoteCount === 2) 	{
+	   	return [stringValue.slice(1,-1), str.slice(i+1)];
+	    }
 
-
-const parseStringValue = value => {
-	const typeArray = [nullParser, booleanParser, numberParser, arrayParser, stringParser];
-	let returnedValue;
-	for (let type of typeArray) {
-		returnedValue = type(value);
-		
-		if(returnedValue !== null) {
-			// console.log(returnedValue);
-			return returnedValue;
-		}
 	}
 	return null;
 }
-let count = 0;
+
+
 const arrayParser = string => {
 	if(string[0] !== "[") return null;
-	if(string[0] === "[") count++;
 	let newArray = new Array();
+	// console.log(newArray);
 	let subString = string.slice(1, string.length);
 	 while ( subString.length !== 0){
-		let returnedValue = parseStringValue(subString.trimStart());
+		let returnedValue = parseJsonArray(subString.trimStart());
 		if( returnedValue  === null) {
-			return null;
+			if(subString.trimStart()[0] === "]") {
+				return [newArray, subString.trimStart().slice(1, subString.trimStart().length)]
+			}
+			return null
 		}
 		newArray.push(returnedValue[0]);
 		// console.log(newArray);
 		if(returnedValue[1].trimStart().startsWith(",")) {
 			subString = returnedValue[1].trimStart().slice(1, returnedValue[1].trimStart().length);
+			// console.log(subString)
 			continue;
 		}
 		if(returnedValue[1].trimStart().startsWith("]")) {
@@ -108,30 +94,84 @@ const arrayParser = string => {
 		}
 		return null;
 	}
+	// return null;
 }
 
-let result = arrayParser(fs.readFileSync("input.json").toString("utf-8"));
-if(result === null  || count > 19 ) {
-	console.log(null);
-} else {
-	console.log(JSON.stringify(result[1].length >= 1? null: arrayParser(fs.readFileSync("input.json").toString("utf-8"))[0]));
+
+
+const objectParser = string => {
+	if(string[0] !== "{") return null;
+	let newObject = new Object();
+	let subString = string.slice(1, string.length);
+	// console.log(subString);
+	while( subString.length !== 0) {
+		let returnedValue =  parseJsonObject(subString.trimStart());
+		if(returnedValue === null) {
+			if(subString.trimStart()[0] === "}") {
+				return [newObject, subString.trimStart().slice(1, subString.trimStart().length)]
+			}
+			return null;
+		} 
+		newObject[returnedValue[0][0]] = returnedValue[0][1];
+		// console.log(newObject);
+		if(returnedValue[1].trimStart().startsWith(",")) {
+			subString = returnedValue[1].trimStart().slice(1, returnedValue[1].trimStart().length);
+			continue;
+		}
+		if(returnedValue[1].trimStart().startsWith("}")) {
+			return [newObject, returnedValue[1].trimStart().slice(1, returnedValue[1].trimStart().length)];
+		}
+		return null;
+	}
 }
 
-const parseJsonObject = value => {
-	let key = stringParser(value);
+
+const parseJsonObject = input => {
+	let key = stringParser(input);
+	// console.log(key);
 	if(key === null) return null;
 	let subString = key[1].trimStart()
 	if(! subString.startsWith(":")) return null;
 	subString = subString.slice(1, subString.length);
 	let value = parseJsonArray(subString.trimStart());
+	// console.log(value);
 	if( value === null) return null;
-	return [[key, value[0]], value[1]]; 
+	return [[key[0], value[0]], value[1]]; 
 
 }
-const objectParser = string => {
-	if(string[0] !== "{") return null;
-	let subString = subString.slice(1, string.length);
-	while( subString.length !== 0) {
-		let returnedValue =  
+const parseJsonArray = value => {
+	// console.log(value)
+	const typeArray = [nullParser, booleanParser, numberParser, stringParser, arrayParser,objectParser ];
+	let returnedValue;
+	for (let type of typeArray) {
+		returnedValue = type(value);	
+		if(returnedValue !== null) {
+			// console.log(returnedValue);
+			return returnedValue;
+		}
 	}
+	return null;
 }
+
+
+let json = fs.readFileSync("input.json").toString("utf-8");
+if(json.startsWith("{")) {
+	let objectResult = objectParser(json);
+	if(objectResult === null) console.log(null);
+	else console.log(JSON.stringify(objectResult[1].length >= 1? null: objectResult[0]));
+} else if( json.startsWith("[")) {
+	let arrayResult = arrayParser(json);
+	if(arrayResult === null) {
+		console.log(null);
+	} else console.log(JSON.stringify(arrayResult[1].length >= 1? null: arrayResult[0]));
+} else console.log(null);
+
+// function callAllParsers(value) {
+// 	for(let type of typeArray) {
+// 		let result = type(value);
+// 		if(result !== null) return result;
+// 	}
+// 	return null;
+// }
+// console.log(JSON.stringify(callAllParsers(fs.readFileSync("input.json").toString("utf-8"))));
+
